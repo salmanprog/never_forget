12145ef272f41d7a52d92e5577dd63f1c3534ce9
Arsalan Ahmed <arsalanahmeddev1@gmail.com>
2026-04-24 22:06:04 +0500

    translator issue fixed

--- resources/views/layouts/website/master.blade.php
+++ resources/views/layouts/website/master.blade.php
@@ -235,10 +235,6 @@
             });
         });
     </script>
-    <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
-
-    
-    
+    <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 </body>
-
 </html>
